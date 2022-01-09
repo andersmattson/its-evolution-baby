e9863bac3c9dc3b637408124a2ce7f3ce5b7dd56
Anders Mattson <anders.mattson@goteborgfilmfestival.se>
2022-01-09 13:10:37 +0100

    Updated dev environment to include source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-	mode: 'production',
-	entry: './src/main.js',
-	output: {
-		path: path.resolve(__dirname, 'docs'),
-		filename: 'bundle.js'
-	},
-	devServer: {
-		static: {
-			directory: path.join(__dirname, 'docs'),
+module.exports = (env) => {
+	
+	if( env.production ){
+		return {
+			mode: 'production',
+			entry: './src/main.js',
+			output: {
+				path: path.resolve(__dirname, 'docs'),
+				filename: 'bundle.js'
+			},
+			devServer: {
+				static: {
+					directory: path.join(__dirname, 'docs'),
+				}
+			},
+			plugins: [
+				new HtmlWebpackPlugin({
+					filename: 'index.html',
+					template: 'template.html',
+					inject: 'body'
+				})
+			]
+		};
+	} else {
+		return {
+			mode: 'development',
+			entry: './src/main.js',
+			devtool: 'inline-source-map',
+			output: {
+				path: path.resolve(__dirname, 'docs'),
+				filename: 'bundle.js'
+			},
+			devServer: {
+				static: {
+					directory: path.join(__dirname, 'docs'),
+				}
+			},
+			plugins: [
+				new HtmlWebpackPlugin({
+					filename: 'index.html',
+					template: 'template.html',
+					inject: 'body'
+				})
+			]
 		}
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: 'template.html',
-			inject: 'body'
-		})
-	]
+	}
 }
